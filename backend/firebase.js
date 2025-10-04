ff2120354df45fc5dbea7e954e2d79962dc716fb
asthajain0 <130166764+asthajain0@users.noreply.github.com>
2025-10-04 22:42:06 +0530

    Update firebase.js

--- backend/firebase.js
+++ backend/firebase.js
@@ -12,9 +12,9 @@ dotenv.config();
 // initialize firebase
 admin.initializeApp({
   credential: admin.credential.cert({
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
 
